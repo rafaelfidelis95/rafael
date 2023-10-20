ef359dd4181ab3e2d97da989829d7133259c9457
rafael <rafaelfidelis694@.com.br>
2023-10-19 22:32:57 -0300

    aula1 js

---
+++ e.js
@@ -0,0 +1,47 @@
+//console.log('olá mundo');//Essa variável pode ser reatribuída
+//let pessoa1 = 'henrique';//Esse tipo de váriavel, não é mais utilizado com boas práticas em 
+//var pessoa2 = 'joao';
+//const pessoa3 = 'lana';
+
+//let mensagem = 'Olá, essa é a mensagem da minha variável';
+//console.log(mensagem);
+//mensagem = 'Essa é a nova mensagem da minha variável';
+//console.log(mensagem);
+
+//float: números que contém pontos ou vírgulas
+//int: números inteiros( não contém pontos ou vírgulas)
+//bool: booleano: true(verdadeiro), false(falso)
+//string: do tipo texto, precisa ser colocado entre aspas duplas ou simples
+
+let num1 = 15;
+let num2 = 20;
+
+//let soma = num1 + num2;
+//console.log(soma);
+
+//let multi = num1 * num2;
+//console.log(multi);
+
+//let divisao = num1 / num2;
+//console.log(divisao);
+
+//let sub = num1 - num2;
+//console.log(sub);
+
+
+//cls: apaga
+
+//if:se
+//: senão
+
+//>:maior que
+//<:menor que
+//>=: maior ou igual
+//<=: menor ou igual
+//===: igual
+
+if(num1 > 10)(
+    console.log('0 número 1 é maior do que 10');
+)else(
+    console.log('0 número 1, não é maior do que 10');
+)
